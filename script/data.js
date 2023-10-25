dc3794c2ff200f5598dbcaab14460868ee83e08a
Dongnguyenbmt1996 <131173013+Dongnguyenbmt1996@users.noreply.github.com>
2023-10-26 00:54:53 +0700

    Add files via upload

---
+++ script/data.js
@@ -0,0 +1,57 @@
+"use strict";
+const btnExport = document.getElementById("export-btn");
+const btnImport = document.getElementById("import-btn");
+const fileInput = document.getElementById("input-file");
+// bắt sự kiện nhấn vào nút export
+btnExport.addEventListener("click", function () {
+  const isExport = confirm("Bạn xác nhận chắc chắn Export?");
+  if (isExport) {
+    saveStaticDataToFile();
+  }
+});
+///////////////////////
+//Hàm lưu dữ liệu xuống file
+function saveStaticDataToFile() {
+  //tạo dữ liệu để lưu xuống
+  const blob = new Blob([JSON.stringify(getFromStorage("petArr"), null, 2)], {
+    type: "application/json",
+  });
+  //lưu file
+  saveAs(blob, "petData.json");
+}
+//////////////////////////
+//bắt sự kiện nút Import
+btnImport.addEventListener("click", function () {
+  //Kiểm tra người dùng đã chọn tập tin chưa?
+  if (!fileInput.value) {
+    alert("Vui lòng chọn file muốn import!");
+  } else {
+    //xác nhận import
+    const isImport = confirm("Bạn có chắc chắn import file này?");
+    if (isImport) {
+      const file = fileInput.file[0];
+      const reader = new FileReader();
+      //sự kiện load dữ liệu file lên
+      reader.addEventListener(
+        "load",
+        function () {
+          //kiểm tra cấu trúc file
+          const isValidateFile = checkFile(JSON.parse(reader.result));
+          if (isValidateFile) {
+            //lưu dữ liệu xuống localstorage
+            saveToStorage("petArr", JSON.parse(reader.result));
+            //thông báo import thành công
+            alert("Import thành công!");
+          }
+        },
+        false
+      );
+      //đọc file
+      if (file) {
+        reader.readAsText(file);
+      }
+      //reset file input
+      fileInput.value = "";
+    }
+  }
+});
---
+++ script/search.js
@@ -0,0 +1,121 @@
+"use strict";
+
+const idInput = document.getElementById("input-id");
+const nameInput = document.getElementById("input-name");
+const ageInput = document.getElementById("input-age");
+const typeInput = document.getElementById("input-type");
+const breedInput = document.getElementById("input-breed");
+const vaccinatedInput = document.getElementById("input-vaccinated");
+const dewormedInput = document.getElementById("input-dewormed");
+const sterilizedInput = document.getElementById("input-sterilized");
+const findBtn = document.getElementById("find-btn");
+let tableBodyEl = document.getElementById("tbody");
+
+const petArr = getFromStorage("petArr") || [];
+const breedArr = getFromStorage("breedArr") || [];
+const formEl = document.getElementById("container-form");
+saveToStorage("petArr", petArr);
+
+// Sự kiện chọn vào typeInput
+function renderBreed() {
+  breedInput.innerHTML = "<option>Select Breed</option>";
+  const breedDogs = breedArr.filter((breeds) => breeds.type === "Dog");
+  const breedCats = breedArr.filter((breeds) => breeds.type === "Cat");
+  if (typeInput.value === "Dog") {
+    breedDogs.forEach(function (breeds) {
+      const option = document.createElement("option");
+      option.innerHTML = `${breeds.breed}`;
+      breedInput.appendChild(option);
+    });
+  } else if (typeInput.value === "Cat") {
+    breedCats.forEach(function (breeds) {
+      const option = document.createElement("option");
+      option.innerHTML = `${breeds.breed}`;
+      breedInput.appendChild(option);
+    });
+  }
+}
+typeInput.addEventListener("click", renderBreed);
+
+// Thêm dữ liệu nhập vào
+function renderTableData(petArr) {
+  tableBodyEl.innerHTML = "";
+  for (let i = 0; i < petArr.length; i++) {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <th scope="row">${petArr[i].id}</th>
+				<td>${petArr[i].name}</td>
+				<td>${petArr[i].age}</td>
+				<td>${petArr[i].type}</td>
+				<td>${petArr[i].weight} kg</td>
+				<td>${petArr[i].length} cm</td>
+				<td>${petArr[i].breed}</td>
+				<td>
+					<i class="bi bi-square-fill" style="color: ${petArr[i].color}"></i>
+				</td>
+				<td>
+          <i class="bi ${
+            petArr[i].vaccinated ? "bi-check-circle-fill" : "bi-x-circle-fill"
+          }"></i>
+        </td>
+				<td>
+          <i class="bi ${
+            petArr[i].dewormed ? "bi-check-circle-fill" : "bi-x-circle-fill"
+          }"></i>
+        </td>
+				<td>
+          <i class="bi ${
+            petArr[i].sterilized ? "bi-check-circle-fill" : "bi-x-circle-fill"
+          }"></i>
+        </td>
+				<td>
+          ${new Date(petArr[i].date).getDate()}/${
+      new Date(petArr[i].date).getMonth() + 1
+    }/${new Date(petArr[i].date).getFullYear()}
+        </td>
+			
+      `;
+
+    tableBodyEl.appendChild(row);
+  }
+}
+renderTableData(petArr);
+
+////////////////////////
+///Bắt sự kiện nhấn nút find
+findBtn.addEventListener("click", function () {
+  let petArrFind = petArr;
+  if (idInput.value) {
+    petArrFind = petArrFind.filter((pet) => pet.id.includes(idInput.value));
+  }
+  //Tìm theo name
+  if (nameInput.value) {
+    petArrFind = petArrFind.filter((pet) => pet.id.includes(nameInput.value));
+  }
+  if (typeInput.value !== "Select Type") {
+    petArrFind = petArrFind.filter((pet) => pet.type === typeInput.value);
+  }
+  if (breedInput.value !== "Select Breed") {
+    petArrFind = petArrFind.filter((pet) => pet.breed === breedInput.value);
+  }
+  if (vaccinatedInput.value) {
+    petArrFind = petArrFind.filter((pet) => pet.vaccinated === true);
+  }
+  if (dewormedInput.value) {
+    petArrFind = petArrFind.filter((pet) => pet.dewormed === true);
+  }
+  if (sterilizedInput.value) {
+    petArrFind = petArrFind.filter((pet) => pet.sterilized === true);
+  }
+  renderTableData(petArrFind);
+});
+//hiển thị các loại giống breed
+renderBreed();
+//hàm hiển thị tất cả các giống breed
+function renderBreed() {
+  breedArr.forEach(function (breedItem) {
+    const option = document.createElement("option");
+    option.innerHTML = `${breedItem.breed}`;
+    breedInput.appendChild(option);
+  });
+}
